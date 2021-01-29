b25fb82414897e4231a1f735e496e716bed9376f
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-01-29 18:56:52 +0600

    for loop

---
+++ forLoop.js
@@ -0,0 +1,17 @@
+// print number from 1 to 10
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(i);
+}
+
+// Print the odd numbers less than 100
+
+for (let i = 1; i <= 100; i += 2) {
+    // console.log(i);
+}
+
+// Print the multiplication table with 7
+
+for (let i = 1; i <= 10; i++) {
+    console.log('7 * ' + i + ' = ' + i * 7);
+}
