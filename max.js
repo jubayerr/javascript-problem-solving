6ba2e2dc28b33246398f0ddeb1d486b6260ad54b
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-02-15 10:47:10 +0600

    find maximum number

---
+++ max.js
@@ -0,0 +1,16 @@
+// Coding challenge #13: Find the maximum number in an array of numbers
+
+function findMax(arr) {
+    var maxNumber = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (element > maxNumber) {
+            maxNumber = element;
+        }
+    }
+    return maxNumber;
+}
+
+var myNumbers = [1, 2, 3, 4, 9, 10, 15, 90];
+var result = findMax(myNumbers);
+console.log(result);
