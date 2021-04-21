2ecbcc0843779e9dfeecaf36651de2d0939e1030
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-04-21 12:58:55 +0600

    get fibonacci numbers

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,10 @@
-// // get fibonacci without recursion
-// var fibo1 = 0;
-// var fibo2 = 1;
+// get fibonacci without recursion
+var fibo1 = 0;
+var fibo2 = 1;
 
-// for (let i = 2; i < 10; i++) {
-//     var fibonacci = fibo2 + fibo1
-//     fibo1 = fibo2
-//     fibo2 = fibonacci
-//     console.log(fibonacci);
-// }
+for (let i = 2; i < 10; i++) {
+    var fibonacci = fibo2 + fibo1
+    fibo1 = fibo2
+    fibo2 = fibonacci
+    console.log(fibonacci);
+}
