70c0c4776fabad8b496057c8c38c0bf639b48c29
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-04-25 20:38:14 +0600

    merge two arrays

---
+++ jsproblem24.js
@@ -0,0 +1,23 @@
+// Coding challenge #24: Create a function that will merge two arrays and return the result as a new array
+
+const numbers1 = [2, 3, 4, 5, 6]
+const numbers2 = [7, 8, 4, 5, 10]
+
+const mergeArray = (num1, num2) => {
+    let numbers3 = []
+
+    for (let i = 0; i < numbers1.length; i++) {
+        const element = numbers1[i];
+        numbers3.push(element)
+    }
+
+    for (let index = 0; index < numbers2.length; index++) {
+        const element = numbers2[index];
+        numbers3.push(element)
+    }
+    return numbers3
+}
+
+const numbers4 = mergeArray(numbers1, numbers2)
+
+console.log(numbers4);
