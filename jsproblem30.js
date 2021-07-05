d08bafc7e904ad79a27ab86ab0bc39c0aa50f1fe
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-07-05 19:39:13 +0600

    js practice

--- jsproblem30.js
+++ jsproblem30.js
@@ -1,7 +1,3 @@
-// Print the multiplication table with 7
-
-
-for (let i = 1; i <=10; i++) {
-    console.log("7"+ ' * ' + i + " = " + 7 * i);
-    
+for (let i = 1; i <= 10; i++) {
+    console.log("7" + "*" + i + "=" + 7 * i);
 }
